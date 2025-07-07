3b4dc04cf61930113aedfe199b836bdd32c89eb2
ingaabbb <ingabaghduashvili7@gmail.com>
2025-07-07 17:57:06 +0400

    .

---
+++ level 002/classwork/cw1.py
@@ -0,0 +1,11 @@
+#1) შექმენით 2 ცვლადი, პირველში შეინახეთ integer,
+# მეორეში კი float ტიპის მნიშნველობები. ჩაატარეთ ყველა მათემატიკური ოპერაცია მათ შორის.
+
+#ეს ოპერაციები აუცილებლად უნდა ჩასვათ print ფუნქციაში
+
+number1=10
+number2=5.5
+print(number1+number2)
+print(number1*number2)
+print(number1/number2)
+print(number1-number2)
---
+++ level 002/homework/hw3.py
@@ -0,0 +1,4 @@
+number = 10;
+word = "Car"
+print(number)   # 10
+print (word)
