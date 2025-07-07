58940dc80fd39c05bdc24d85ae1d4f2e80e668b4
ingaabbb <ingabaghduashvili7@gmail.com>
2025-07-07 17:50:19 +0400

    Update hw4.py

--- level 002/homework/hw4.py
+++ level 002/homework/hw4.py
@@ -2,4 +2,8 @@ name= "Inga"
 surname="baghduashvili"
 age=18
 print(name+surname)
+print(num1+num2)
 
+# რადგან name surname ერთნაირი ტიპის მონაცემია ანუ string ია, print(name+surname) გამოიტანს მნიშვნელობას სადაც name surname გაერთიანებულია.
+# integer ტიპის მონაცემებს თუ ჩავწერთ print ში ანუ print(num1+num2) გამოიტანს num1 და num2 რიცხვების ჯამს. 
+# მაგრამ სხვადასხვა ტიპის მონაცემზე  ვერ ხვდება რა შედეგი უნდა გამოიტანოს. 
