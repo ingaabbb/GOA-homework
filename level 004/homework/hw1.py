3236e1942db00d1c6244eb2007b4182b11f7b115
ingaabbb <ingabaghduashvili7@gmail.com>
2025-07-13 01:23:55 +0400

    Create hw1.py

---
+++ level 004/homework/hw1.py
@@ -0,0 +1,25 @@
+#Task 2
+# debugging არის კოდში შეცდომის პოვნა და გამოსწორება
+
+#Task 3 
+#კონკადინაცია არის სტრინგების შეერთება.მაგ:
+
+word1="Happy "
+word2="birthday"
+word3=word1+word2
+
+print("Inga "+ "baghduashvili")
+
+saxeli="inga"
+print("Hello "+"my name "+ "is "+saxeli)
+
+#Task 4
+num1=4
+num2=6
+num3=3
+num4=7
+num5=5
+jami=num1+num2+num3+num4+num5
+sashualo=jami/5
+
+print(sashualo)
