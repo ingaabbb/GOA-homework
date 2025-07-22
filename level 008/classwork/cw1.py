063441efae33ac32c962c10d810eeb7a6b5ea070
ingaabbb <ingabaghduashvili7@gmail.com>
2025-07-22 12:40:51 +0400

    .

---
+++ level 008/classwork/cw1.py
@@ -0,0 +1,9 @@
+
+sigrdze= int(input("shemoitanet martkutxedis sigrdze "))
+sigane= int(input("shemoitanet martkutxedis sigane "))
+perimetri=2*(sigrdze+sigane)
+print(perimetri)
+
+asaki=int(input("daweret tqveni asaki"))
+print("ati wlis shemdeg momxmareblis asaki iqneba  ")
+print(asaki+10)
