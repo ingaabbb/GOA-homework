43d8534d1b083d66f9a33937ca7ab3017b7296d8
ingaabbb <ingabaghduashvili7@gmail.com>
2025-07-25 21:22:04 +0400

    .

---
+++ level 010/classwork/cw1.py
@@ -0,0 +1,27 @@
+# Task 1 
+# explicity  არის , როდესაც კომპიუტერს ჩვენ ვეუბნებით მონაცემთა რომელ ტიპზე უნდა იმუშავოს. მაგ: float(12)
+# implicity არის, როდესაც კომპიუტერი თავად ხვდება, მონაცემთა რომელი ტიპი უნდა შეინახოს. მაგ: num=10/5  თვითონ ხვდება,
+# რომ float უნდა შეინახოს. 
+
+# Task 2
+saxeli= "Inga"
+asaki= 18
+print("my name is " + saxeli + ". I'm " +str(asaki)+ " years old." )
+
+# Task 3
+fav_num=int( input("shemoitanet sayvareli ricxvi"))
+print(fav_num**5)
+
+# Task 4 
+# if- შესრულდება, თუ სრულდება if-ის პირობა.
+# elif - შესრულდება, თუ არ სრულდება if -ის პირობა, მაგრამ elif-ის სრულდება
+# else - შესრულდება, თუ ზემოთხსენებული არცერთი არ შესრულდება. 
+
+ricxvi= 12
+
+if ricxvi < 10:
+    print("ricxvi atze naklebia")
+elif ricxvi < 15:
+    print("ricxvi metia 10-ze da naklebia 15 -ze")
+else: print("ricxvi 15-ze metia")
+# აქ შესრულდება else 
