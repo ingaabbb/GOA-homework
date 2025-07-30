aa33ef261580997d2b10f19f2054c4a537e3b7cb
ingaabbb <ingabaghduashvili7@gmail.com>
2025-07-30 15:33:25 +0400

    .

---
+++ level 012/classwork/cw1.py
@@ -0,0 +1,20 @@
+# 1) დაწერეთ 2-2 მაგალითი შედარების ოპერატორებზე(>, <, <=, >=, ==, !=)
+
+# 2) დაწერეთ მაგალითები შედარების ოპერატორებზე.
+
+# 3) სანამ ამ კოდს გაუშვებდეთ, კარგად იფიქრეთ ამის პასუხზე(პასუხი იქნება ერთ-ერთი, ორი Boolean მნიშვნელობებიდან True/False)
+
+# print(True and False or 5 > 10 and 100 / 5 > 10 and "Python" == "Python" or 2 * 3 < 5 or 6 + 7 - 9 < 20 and True and 5 > 5.0 or 10 - 10 < 100)
+
+# Task 1
+print(5 > 10)  #false
+print(15 < 25)  #true
+print(25 >= 25)  #true
+print(30 <= 30)  #true
+print(5 == 4)  #false
+print( 3 != 9)  #true
+print("bla")
+print(5>5.0)
+
+print(True and False or 5 > 10 and 100 / 5 > 10 and "Python" == "Python" or 2 * 3 < 5 or 6 + 7 - 9 < 20 and True and 5 > 5.0 or 10 - 10 < 100)
+ #        False      or False  and    True      and    True              or  False    or     True       and True and False   or True  
