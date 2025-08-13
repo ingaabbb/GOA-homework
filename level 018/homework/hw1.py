7e1e0105d7328072150aa08cf67f637e4825e47d
ingaabbb <ingabaghduashvili7@gmail.com>
2025-08-14 00:31:01 +0400

    .

---
+++ level 018/homework/hw1.py
@@ -0,0 +1,28 @@
+# 2) მოიძიეთ for ციკლზე ინფორმაცია და შეასრულეთ რამოდენიმე მაგალითი
+
+# 3) for loop-ის გამოყენებით დაბეჭდეთ თქვენი სახელი 10-ჯერ
+
+# 4) მომხმარებელს შემოატანინეთ 5 რიცხვი for loop-ის გამოყენებით გაიგეთ მათი საშუალო არითმეტიკული
+
+# 5) ივარჯიშეთ if/elif/else-ებზე და გააკეთეთ რამოდენიმე მაგალითი მათზეც
+
+# Task 2
+# მაგალითი 1
+for i in range(5):
+    print("3")
+
+# მაგალითი 2
+for i in range (5):
+    feri=input("დაწერეთ სამი საყვარელი ფერი")
+    print(feri)
+
+# Task 3
+for i in range(10):
+    print("inga")
+
+# Task 4
+jami=0
+for i in range(5):
+    num=int(input("shemoitanet ricxvi"))
+    jami+=num
+print(jami/5)
