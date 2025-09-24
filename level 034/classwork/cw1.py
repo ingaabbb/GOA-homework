ca1ddee8f7b11c328ba54f0d1d1aef591d7523eb
ingaabbb <ingabaghduashvili7@gmail.com>
2025-09-24 23:07:12 +0400

    .

--- level 034/classwork/cw1.py
+++ level 034/classwork/cw1.py
@@ -1,19 +1,12 @@
-# def greet():
-#     name=input("daweret tkveni saxeli")
-#     print("Hello world!")
-#     print("Hello "+ name)
+def greet():
+    name=input("daweret tkveni saxeli")
+    print("Hello world!")
+    print("Hello "+ name)
 
-# greet()
+greet()
 
-# 1) შექმენით len ფუნქციის კლონი. Custom ფუნქციების დახმარებით.
-# ანუ უნდა შექმნათ ფუნქცია, რომელსაც გადაეცემა ერთი არგუმენტი, ეს იქნება სია ან string-ი. თქვენი დავალებაა 
-# გადაცემული სიის/string-ის სიგრძე ანუ ელემენტთა რაოდენობა დააბრუნოთ.
-
-# შეგიძლიათ შექმნათ ცვლადი, რომელსაც თავიდან გაუტოლებთ 0-ს. შემდეგ for ციკლით გადაუაროთ მიღებულ ელემენტს 
-# და დაითვალოთ რამდენი ელემენტია მასში ამ ცვლადის ყოველი იტერაციისას ერთით გაზრდით
-
+# 1) შექმენით len ფუნქციის კლონი.
 # 2) შექმენით find ფუნქციის(string ფუნქციის) კლონი
-
 # 3) შექმენით range ფუნქციის კლონი
 
 # Task 1
@@ -23,19 +16,30 @@ def kloni1(argumenti):
         number+=1
     print(number)
 
-kloni1("world")
+
 
 # Task 2
-print("hi")
+
 def kloni2(word, sadziebo):
     counter=0
     while counter<len(word):
         if word[counter]==sadziebo:
-          return counter
-          counter+=1
-    return -1
+          print (counter)
+        counter+=1
+    print(-1) 
 
 
 kloni2("world","r")
 
-print("hello")
+
+
+def find_clone(text,char):
+    result=-1
+    index=0
+
+    for i in text:
+        if i== char and result==-1 :
+            result=index
+        index += 1
+    print(result)
+
