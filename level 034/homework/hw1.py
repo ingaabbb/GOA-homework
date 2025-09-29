edadc92dfcbc43fee8d8cfeb3bcfe458b923dd28
ingaabbb <ingabaghduashvili7@gmail.com>
2025-09-29 17:07:11 +0400

    .

--- level 034/homework/hw1.py
+++ level 034/homework/hw1.py
@@ -1,3 +1,12 @@
 # 4) უყურეთ ჩანაწერს თავიდან და გააკეთეთ ფუნქციების შექმნის რამოდენიმე მაგალითი
 
 # 5) შეეცადეთ რომ დაასრულოთ საკლასო დავალება
+
+# Task 4
+def gamarjoba(name):
+    return("gamarjoba {name}")
+
+def SamiRicxvisSashualo(num1,num2,num3):
+    return ((num1 +num2 +num2)/3)
+
+# Task 5
